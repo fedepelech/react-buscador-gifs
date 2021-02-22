4b448fe3c9450883a491ac8d48bcad1837be8fc2
fede pelech <federicop@grava.io>
2021-02-21 22:44:04 -0300

    cambios minimos

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -7,9 +7,12 @@ import { Container, Alert, Card, CardImg, Row, Col } from 'reactstrap';
 const GifExpertApp = () => {
   const [categories, setCategory] = useState([]);
   const [image, setImage] = useState('');
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     getGifWelcome()
     .then((image) => {
+      setLoading(false);
       setImage(image);
     })
   }, []);
@@ -19,24 +22,28 @@ const GifExpertApp = () => {
       <h2>Buscador de Gif's</h2>
       <AddCategory setCategory={setCategory} />
       <hr />
-      {categories.length === 0 && 
-        <>
-          <Alert color="secondary">
-            <h3 style={{textAlign:'right'}}>Bienvenidx!</h3>
-            <p style={{textAlign:'right'}}>Ingrese algo en el buscador.</p>
-          </Alert>
-          <Row>
-            <Col xs="4">
-          <Card
-            body
-            className="animate__animated animate__fadeInUp card-custom"
-          >
-            <CardImg src={image.url} alt="image.title" />
-          </Card>
-            </Col>
-          </Row>
-        </>
-      }
+      <div>
+        {categories.length === 0 && (
+          <>
+            <Alert color="secondary">
+              <h3 style={{ textAlign: "right" }}>Bienvenidx!</h3>
+              <p style={{ textAlign: "right" }}>Ingrese algo en el buscador.</p>
+            </Alert>
+            {loading && <h3 style={{color: 'white', textAlign:'center'}}>Cargando...</h3>}
+            <Row>
+              <Col xs="4">
+                <Card
+                  body
+                  className="animate__animated animate__fadeInUp card-custom"
+                >
+                  <CardImg src={image.url} alt="image.title" />
+                </Card>
+              </Col>
+            </Row>
+          </>
+        )}
+      </div>
+
       <ol>
         {categories.map((category) => {
           return <GifGrid key={category} category={category} />;
